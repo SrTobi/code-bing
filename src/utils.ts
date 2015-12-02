35ebfd112d0dcbd1f35c992ca1f70c744a333d8c
Jacob Hands <g@gstx.co>
2015-12-02 16:08:47 -0700

    Semicolon fail

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ export function isNullOrEmpty(s: string): boolean {
 	return s == null || s == "";
 }
 export function startsWith(s: string, startsWith: string) {
-	return s != null && startsWith != null && s.indexOf(startsWith) == 0
+	return s != null && startsWith != null && s.indexOf(startsWith) == 0;
 }
