cc997a50ff2f1da06b7a09b2e99249b3e2e3c7d9
Jacob Hands <g@gstx.co>
2015-12-07 11:08:25 -0700

    Add configuration tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -10,13 +10,28 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
-
+import * as cInfo from '../src/configinfo'
 // Defines a Mocha test suite to group tests of similar kind together
+/*
 suite("Extension Tests", () => {
 
 	// Defines a Mocha unit test
 	test("Something 1", () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
 		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
+});
+*/
+suite("Configuration Tests:", () => {
+	let config = vscode.workspace.getConfiguration(cInfo.group);
+	test("configInfo.keys only contains valid config keys", function() {
+		for (let key in cInfo.keys) {
+			if (cInfo.keys.hasOwnProperty(key)) {
+				assert(config.has(key), "Invalid configInfo.key: " + key)
+			}
+		}
+	});
+	test("newCommandString properly formats string", function(){
+		assert.equal("codebing.search", cInfo.newCommandString("search"));
 	});
 });
